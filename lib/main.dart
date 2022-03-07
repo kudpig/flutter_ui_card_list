0924d2e6ad1f16bd2b721cad3e6b7b3b15cadbf7
kudpig <shin.kudo.aa@gmail.com>
2022-03-08 02:55:21 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -29,40 +29,126 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
 
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    final double height = MediaQuery.of(context).size.height;
+    final double width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column( // 縦要素
+        children: [
+          Container(
+            height: 230,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(50)
+              ),
+              color: Color(0xFF363f93),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            child: Stack( // コンテナの上に子要素として載せるイメージ
+              children: [
+                Positioned(
+                    top: 80,
+                    left: 0,
+                    child: Container(
+                      height: 100,
+                      width: 300,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(50),
+                          bottomRight: Radius.circular(50),
+                        )
+                      ),
+                    )
+                ),
+                const Positioned(
+                    top: 115,
+                    left: 20,
+                    child: Text('Your Books', style: TextStyle(fontSize: 30, color: Color(0xFF363f93))))
+              ],
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+          ),
+          SizedBox(height: height*0.05,),
+          Container(
+            height: 230,
+            child: Stack(
+              children: [
+                Positioned(
+                    top: 35,
+                    left: 20,
+                    child: Material(
+
+                      child: Container(
+                        height: 180,
+                        width: width*0.9,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(0.0),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.grey.withOpacity(0.3),
+                                offset: const Offset(-10.0, 10.0),
+                                blurRadius: 20.0,
+                                spreadRadius: 4.0
+                            ),
+                          ],
+                        ),
+                      ),
+
+                )),
+                Positioned(
+                  top: 0,
+                    left: 30,
+                    child: Card(
+                      elevation: 10.0,
+                      shadowColor: Colors.grey.withOpacity(0.5),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: Container(
+                        height: 200,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          color: Color(0xFF363f93),
+                          borderRadius: BorderRadius.circular(10.0),
+                          image: const DecorationImage(
+                            fit: BoxFit.fill,
+                            image: AssetImage('img/101.png'),
+                          )
+                        ),
+                      ),
+                    )
+                ),
+                Positioned(
+                    top: 60,
+                    left: 200,
+                    child: Container(
+                      height: 150,
+                      width: 160,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text('Big Miracles', style: TextStyle(fontSize: 20, color: Color(0xFF363f93), fontWeight: FontWeight.bold),),
+                          Text('Dylan Ahmed', style: TextStyle(fontSize: 16, color: Colors.grey, fontWeight: FontWeight.bold),),
+                          Divider(color: Colors.black),
+                          Text('Its larger than life ', style: TextStyle(fontSize: 16, color: Colors.grey, fontWeight: FontWeight.bold),),
+                        ],
+                      ),
+                    )
+                )
+
+              ],
+            ),
+          )
+
+
+
+        ],
+      )
     );
   }
 }
